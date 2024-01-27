cd5a839174ca0b19b37a3fcedb09e26442805416
Luji <zlfjrii@gmail.com>
2024-01-27 20:43:04 +0700

    learn python: how to define floating point on python and about how python not support mixing operators between numbers and strings

--- basic/variablesAndTypes.py
+++ basic/variablesAndTypes.py
@@ -1,4 +1,18 @@
 # Python is completely object-oriented but not "staticaly typed". All variables in python are object.
 myInt = 7
 # myInt is a variable. In python, it's an object
-print(myInt)
+# print(myInt)
+
+# if i want to define a floating number:
+
+myFloat = 7.0
+# or
+myFloat2 = float(7.0)
+# print(myFloat)
+# print(myFloat2)
+
+# mixing operators between numbers and strings are not supported!
+num = 1
+word = "one"
+
+print(num + word)
