3e9687def51df56d71b07e30356f90545f7b725d
Luji <zlfjrii@gmail.com>
2024-01-27 20:25:17 +0700

    Learn python basics: Hello World, indentation, variables and types in python.

---
+++ basic/indentation.py
@@ -0,0 +1,4 @@
+# it says that python uses indentation for block instead of curly braces.
+x = 1
+if x == 1:
+    print("x is 1")
---
+++ basic/variablesAndTypes.py
@@ -0,0 +1,4 @@
+# Python is completely object-oriented but not "staticaly typed". All variables in python are object.
+myInt = 7
+# myInt is a variable. In python, it's an object
+print(myInt)
