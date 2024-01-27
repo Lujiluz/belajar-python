99185412dc7435da09c1e5179ff0ff931abc6d59
Luji <zlfjrii@gmail.com>
2024-01-27 20:57:54 +0700

    learn python: operator overloading

---
+++ basic/operatorOverloading.py
@@ -0,0 +1,8 @@
+# python offers a "trick" where, operators like * and + have a different meaning depending on
+# what kind of thing they're applied to. (The technical term for this is operator overloading)
+
+spam_amount = 4
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+# Question: Kenapa gua bisa campur operasi aritmatik antara string dan int hanya jika dikali, kalo ditambah gabisa?
