84e3e34381e80eb4ee0700e9095cf7fe64ea47db
Luji <zlfjrii@gmail.com>
2024-01-28 19:46:24 +0700

    Learn Python: Learn type of operators in python

--- basic/operatorOverloading.py
+++ basic/operatorOverloading.py
@@ -6,3 +6,5 @@ viking_song = "Spam " * spam_amount
 print(viking_song)
 
 # Question: Kenapa gua bisa campur operasi aritmatik antara string dan int hanya jika dikali, kalo ditambah gabisa?
+# Answer: karena python tidak tahu cara mengubah angka menjadi string meggunakan operator +. Kalo operator * kan cuma 
+# mengalikan string berdasarkan angka. Jadi tidak ada perubahan tipe data
---
+++ basic/operators.py
@@ -0,0 +1,44 @@
+"""
+Operators in python:
+
+a + b => {
+    Name: Addition
+    Description: sum of a and b
+}
+a - b => {
+    Name: Substraction
+    Description: Difference of a and b
+}
+a * b => {
+    Name: Multiplication
+    Description: Product of a and b
+}
+a / b => {
+    Name: True division
+    Description: Quotient of a and b
+}
+a // b => {
+    Name: Floor division
+    Description: Quotient of a and b, removign fractional parts
+}
+a % b => {
+    Name: Modulus
+    Description: Integer remainder after division of a by b
+}
+a ** b => {
+    Name: Exponentiation
+    Description: a raised to the power of b
+}
+-a => {
+    Name: Negation
+    Description: negative of a
+}
+"""
+
+# Difference between true division and floor division:
+
+# true division
+print(5 / 2)
+
+# floor division
+print(5 // 2)
